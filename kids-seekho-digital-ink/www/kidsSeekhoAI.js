0b0cfa6a565b90e70e18c31ea4fae6c4f407c2a9
malaramofficial <malaramofficial@gmail.com>
2026-09-06 16:23:01 +0530

    Expose native visual AI to Kids Seekho

--- kids-seekho-digital-ink/www/kidsSeekhoAI.js
+++ kids-seekho-digital-ink/www/kidsSeekhoAI.js
@@ -1,18 +1,7 @@
-var exec = require('cordova/exec');
-
-module.exports = {
-  prepare: function(languageTag, success, failure) {
-    exec(success, failure, 'KidsSeekhoAI', 'prepare', [languageTag || 'en-US']);
-  },
-  recognize: function(strokes, languageTag, width, height, success, failure) {
-    exec(success, failure, 'KidsSeekhoAI', 'recognize', [{
-      strokes: strokes || [],
-      languageTag: languageTag || 'en-US',
-      width: Number(width) || 1,
-      height: Number(height) || 1
-    }]);
-  },
-  isAvailable: function(success, failure) {
-    exec(success, failure, 'KidsSeekhoAI', 'isAvailable', []);
-  }
+var exec=require('cordova/exec');
+module.exports={
+ prepare:function(tag,ok,fail){exec(ok,fail,'KidsSeekhoAI','prepare',[tag||'en-US']);},
+ recognize:function(strokes,tag,w,h,ok,fail){exec(ok,fail,'KidsSeekhoAI','recognize',[{strokes:strokes||[],languageTag:tag||'en-US',width:Number(w)||1,height:Number(h)||1}]);},
+ labelImage:function(base64,ok,fail){exec(ok,fail,'KidsSeekhoAI','labelImage',[{base64:base64||''}]);},
+ isAvailable:function(ok,fail){exec(ok,fail,'KidsSeekhoAI','isAvailable',[])}
 };
